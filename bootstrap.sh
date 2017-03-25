4cebf995b1d9c3fb363773bf6f45f5e58d3aca15
masatakanabeshima <xpuchinya@gmail.com>
2017-03-25 16:52:50 +0900

    change

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cp -f ${JIRA_INSTALL}/conf/server.xml.org ${JIRA_INSTALL}/conf/server.xml
-sed -i -e "4s/%proxyName%/${JIRA_PROXY_NAME}/" ${JIRA_INSTALL}/conf/server.xml
-sed -i -e "4s/%proxyPort%/${JIRA_PROXY_PORT}/" ${JIRA_INSTALL}/conf/server.xml
-sed -i -e "4s/%scheme%/${JIRA_SCHEME}/" ${JIRA_INSTALL}/conf/server.xml
-sed -i -e "4s/%secure%/${JIRA_SECURE}/" ${JIRA_INSTALL}/conf/server.xml
+sed -i -e "s/%proxyName%/${JIRA_PROXY_NAME}/g" ${JIRA_INSTALL}/conf/server.xml
+sed -i -e "s/%proxyPort%/${JIRA_PROXY_PORT}/g" ${JIRA_INSTALL}/conf/server.xml
+sed -i -e "s/%scheme%/${JIRA_SCHEME}/g" ${JIRA_INSTALL}/conf/server.xml
+sed -i -e "s/%secure%/${JIRA_SECURE}/g" ${JIRA_INSTALL}/conf/server.xml
 
 ${JIRA_INSTALL}/bin/catalina.sh run
